8e7ed205128e21febb6e05fa766a7e5f7292eca6
zainudinsainal <supernova37@gmail.com>
2018-02-22 22:05:37 +0800

    Fix js bugs for tweets textbox

--- app/assets/javascripts/button.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  if ($("#textbox").val() == "") $("#submitButtonId").attr("disabled", true);
-
-  $("#textbox").keyup(function() {
-    if ($("#textbox").val() != "") $("#submitButtonId").attr("disabled", false);
-    else $("#submitButtonId").attr("disabled", true);
-  });
-});
