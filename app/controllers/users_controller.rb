d780a42ed6a474124238fb76b5a4b6e19fcb66c3
Liyan <liyan@liyanchua.com>
2018-02-22 15:52:18 +0800

    update followings and followers views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def followings
+    @user  = User.find(params[:id])
+    @users = @user.followings
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.followers
+  end
+
   private
 
   def user_params
