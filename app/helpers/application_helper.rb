031bc73d24f2c6023415d4ab1be567862c6a542e
zainudinsainal <supernova37@gmail.com>
2018-02-22 13:55:54 +0800

    add default carrierwave picture

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,4 @@ module ApplicationHelper
     user == current_user
   end
 
-  def display_avatar(user)  
-    unless user.avatar.nil? 
-      image_tag(user.avatar.file_name) 
-    else
-      image_tag("/assets/images/picture.jpg")
-     end    
-  end
-
 end
