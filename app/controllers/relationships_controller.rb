a5b8e363cab8fe8881b3f4532dd95b882a6d335c
Liyan <liyan@liyanchua.com>
2018-02-22 16:22:16 +0800

    js for follow and unfollow buttons

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,14 +4,12 @@ class RelationshipsController < ApplicationController
     @user = User.find(params[:user_id])
     new_follow = Relationship.new
 		new_follow.update(follower_id: current_user.id, following_id: @user.id)
-    redirect_to users_path
   end
 
   def destroy
     @user = User.find(params[:user_id])
 		unfollow = Relationship.find(params[:id])
 		unfollow.destroy
-    redirect_to users_path
   end
 
 end
