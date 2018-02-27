2ebc65d7da3bb83922992db92e497aabf2f31385
Liyan <liyan@liyanchua.com>
2018-02-27 16:34:53 +0800

    update likes controller and index views

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,7 @@ class LikesController < ApplicationController
 	def create
 		@like = Like.all
 		@tweet = Tweet.find(params[:tweet_id])
-		tweet_likes = tweet.likes.build
+		tweet_likes = @tweet.likes.build
 		tweet_likes.user = current_user
 		tweet_likes.save
 
@@ -20,11 +20,7 @@ class LikesController < ApplicationController
 	private
 
 	def like_params
-		params.require(:like).permit(:tweet_id)
-	end
-
-	def tweet
-		Tweet.find(params[:tweet_id])
+		params.require(:like).permit(:tweet)
 	end
 
 end
